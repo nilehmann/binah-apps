e2a6b11b628f69b9f975a6cfe08d9bcfdc35c60f
Nico Lehmann <nico.lehmannm@gmail.com>
2020-06-03 13:25:11 -0700

    Add S3 presigned url

--- covid/server/src/Server.hs
+++ covid/server/src/Server.hs
@@ -10,7 +10,6 @@ module Server
     )
 where
 
-import qualified Control.Concurrent.MVar       as MVar
 import           Control.Monad.IO.Class         ( MonadIO(..) )
 import           Control.Monad.Reader           ( MonadReader(..)
                                                 , ReaderT(..)
@@ -27,6 +26,7 @@ import           Network.Mime
 import           Frankie.Config
 import           Frankie.Auth
 import qualified Data.Text                     as T
+import qualified Data.Text.Encoding            as T
 
 import           Data.Pool                      ( Pool )
 import qualified Data.Pool                     as Pool
@@ -37,6 +37,8 @@ import           Control.Monad.Trans.Control    ( MonadBaseControl(..)
 import           Control.Monad.Trans.Class      ( lift )
 import           Control.Monad.Logger           ( runNoLoggingT )
 import qualified Control.Concurrent.MVar       as MVar
+import           Control.Lens.Lens              ( (&) )
+import           Control.Lens.Operators         ( (^.) )
 import qualified Text.Mustache.Types           as Mustache
 
 
@@ -54,6 +56,8 @@ import           Controllers.Room
 import           Model
 import           Auth
 
+import qualified Network.AWS                   as AWS
+import qualified Network.AWS.S3                as S3
 
 
 {-@ ignore runServer @-}
@@ -61,11 +65,12 @@ runServer :: HostPreference -> Port -> IO ()
 runServer h p = runNoLoggingT $ do
     templateCache <- liftIO $ MVar.newMVar mempty
     pool          <- createSqlitePool "db.sqlite" 1
+    aws           <- liftIO getAwsConfig
     liftIO . runFrankieServer "dev" $ do
         mode "dev" $ do
             host h
             port p
-            initWith $ initFromPool templateCache pool
+            initWith $ initFromPool aws templateCache pool
         dispatch $ do
             post "/api/signin" signIn
             post "/api/signup" signUp
@@ -77,8 +82,20 @@ runServer h p = runNoLoggingT $ do
             post "/api/room"          roomPost
             post "/api/room/leave"    leaveRoom
             post "/api/room/:id/join" joinRoom
+            get "/api/signurl" s3SignedURL
 
             fallback (sendFromDirectory "static" "index.html")
+
+getAwsConfig :: IO AWSConfig
+getAwsConfig = do
+    let accessKey = "AKIA2SO5WDU4VOFM2Q7F"
+    let secretKey = "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"
+    env <- AWS.newEnv $ AWS.FromKeys (AWS.AccessKey $ T.encodeUtf8 accessKey)
+                                     (AWS.SecretKey $ T.encodeUtf8 secretKey)
+    return $ AWSConfig { awsAuth   = env ^. AWS.envAuth
+                       , awsRegion = AWS.NorthCalifornia
+                       , awsBucket = S3.BucketName "binah-social-distancing"
+                       }
 
 {-@ ignore initDB @-}
 initDB :: IO ()
@@ -105,9 +122,13 @@ sendFile path = do
 -- TODO find a way to provide this without exposing the instance of MonadBaseControl
 
 initFromPool
-    :: MVar.MVar Mustache.TemplateCache -> Pool SqlBackend -> Controller () -> ControllerT TIO ()
-initFromPool cache pool controller = Pool.withResource pool $ \sqlBackend ->
-    configure (Config sqlBackend authMethod cache) . reading backend . unTag $ controller
+    :: AWSConfig
+    -> MVar.MVar Mustache.TemplateCache
+    -> Pool SqlBackend
+    -> Controller ()
+    -> ControllerT TIO ()
+initFromPool aws cache pool controller = Pool.withResource pool $ \sqlBackend ->
+    configure (Config sqlBackend authMethod cache aws) . reading backend . unTag $ controller
 
 instance MonadBase IO TIO where
     liftBase = TIO
