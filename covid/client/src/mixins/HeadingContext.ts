b6a46e9633a3a9bf311a7892f6972eaeaee093af
Nico Lehmann <nico.lehmannm@gmail.com>
2020-08-04 09:55:44 -0700

    Update Binah + Remove covid

--- covid/client/src/mixins/HeadingContext.ts
+++
@@ -1,7 +0,0 @@
-import { Vue, Component, Prop } from "vue-property-decorator";
-
-@Component
-export default class HeadingContext extends Vue {
-  @Prop({ default: 1 })
-  readonly headingContext!: number;
-}
